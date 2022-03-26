17fa82617d9cd2681b95349e750c7fe5fc036407
Ayiks <cliffysarp@gmail.com>
2022-03-26 11:09:01 +0000

    refactor

--- lib/widgets/icon_and_text_widget.dart
+++ lib/widgets/icon_and_text_widget.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/small_text.dart';
 
 class IconAndTextWidget extends StatelessWidget {
   final IconData icon;
   final String text;
-  
+
   final Color iconColor;
 
   const IconAndTextWidget(
       {Key? key,
       required this.icon,
       required this.text,
-      
       required this.iconColor})
       : super(key: key);
 
@@ -19,8 +19,14 @@ class IconAndTextWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(icon, color: iconColor,),
-        SizedBox(width: 5,),
+        Icon(
+          icon,
+          color: iconColor,
+          size: Dimensions.iconSize24,
+        ),
+        SizedBox(
+          width: 5,
+        ),
         SmallText(text: text)
       ],
     );
