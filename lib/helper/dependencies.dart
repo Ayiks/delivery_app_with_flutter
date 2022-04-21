cd2a462989efaaabfc203759a80c6742fc864dea
Ayiks <cliffysarp@gmail.com>
2022-04-21 17:55:39 +0000

    dependencies

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,16 @@
-Future<void> init() async{
-  
+import 'package:food_delivery/controllers/popular_product_controller.dart';
+import 'package:food_delivery/data/api/api_client.dart';
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:food_delivery/utils/app_constant.dart';
+import 'package:get/get.dart';
+
+Future<void> init() async {
+  //load the apiclient
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL));
+
+  //load repository
+  Get.lazyPut(() => PopularProductRepo(apiClient: Get.find()));
+
+  //load controllers
+  Get.lazyPut(() => PopularProductController(popularProductRepo: Get.find()));
 }
