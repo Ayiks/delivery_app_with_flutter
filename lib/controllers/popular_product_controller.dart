c0f0434e8841d2bde60aa8630667b1056957cca6
Ayiks <cliffysarp@gmail.com>
2022-04-21 17:53:44 +0000

    popular product controller

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,23 @@
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
+import '../models/products_model.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
+
+  List<dynamic> _popularProductsList = [];
+  List<dynamic> get popularProductList => _popularProductsList;
+
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+    if (response.statusCode == 200) {
+      print('got products');
+      _popularProductsList = [];
+      _popularProductsList.addAll(Product.fromJson(response.body).products);
+      print(_popularProductsList);
+      update();
+    } else {}
+  }
+}
