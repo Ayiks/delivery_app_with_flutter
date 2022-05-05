4406e601a64ef9da84ee487e861ff6748766b342
Ayiks <cliffysarp@gmail.com>
2022-05-05 22:20:22 +0000

    05

--- lib/pages/home/food_page_body.dart
+++ lib/pages/home/food_page_body.dart
@@ -1,11 +1,13 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:food_delivery/controllers/popular_product_controller.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/utils/dimension.dart';
 import 'package:food_delivery/widgets/app_column.dart';
 import 'package:food_delivery/widgets/big_text.dart';
 import 'package:food_delivery/widgets/icon_and_text_widget.dart';
 import 'package:food_delivery/widgets/small_text.dart';
+import 'package:get/get.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -39,27 +41,32 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-          // color: Colors.redAccent,
-          height: Dimensions.pageView,
-          child: PageView.builder(
-              controller: pageController,
-              itemCount: 5,
-              itemBuilder: (context, position) {
-                return _buildPageItem(position);
-              }),
-        ),
-        DotsIndicator(
-          dotsCount: 5,
-          position: _currentPageValue,
-          decorator: DotsDecorator(
-              color: Colors.black87, // Inactive color
-              activeColor: AppColors.mainColor,
-              activeSize: Size(18.0, 9.0),
-              size: Size.square(9.0),
-              activeShape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10))),
-        ),
+        //slider section
+        GetBuilder<PopularProductController>(builder: (popularProducts) {
+          return Container(
+            // color: Colors.redAccent,
+            height: Dimensions.pageView,
+            child: PageView.builder(
+                controller: pageController,
+                itemCount: popularProducts.popularProductList.length,
+                itemBuilder: (context, position) {
+                  return _buildPageItem(position);
+                }),
+          );
+        }),
+        GetBuilder<PopularProductController>(builder: (popularProducts) {
+          return DotsIndicator(
+            dotsCount: popularProducts.popularProductList.length,
+            position: _currentPageValue,
+            decorator: DotsDecorator(
+                color: Colors.black87, // Inactive color
+                activeColor: AppColors.mainColor,
+                activeSize: Size(18.0, 9.0),
+                size: Size.square(9.0),
+                activeShape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10))),
+          );
+        }),
         SizedBox(
           height: Dimensions.height30,
         ),
