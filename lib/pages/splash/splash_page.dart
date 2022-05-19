69dcd8b1f9d5d477340e3878e0f683f174c2a33a
Ayiks <cliffysarp@gmail.com>
2022-05-19 12:06:54 +0000

    loaded dependencies at the splash screen

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -3,7 +3,11 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/routes/route_helper.dart';
+import 'package:food_delivery/utils/dimension.dart';
 import 'package:get/get.dart';
+
+import '../../controllers/popular_product_controller.dart';
+import '../../controllers/recommended_product_controller.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -17,9 +21,15 @@ class _SplashScreenState extends State<SplashScreen>
   late Animation<double> animation;
   late AnimationController controller;
 
+  _loadResources() async {
+   await Get.find<PopularProductController>().getPopularProductList();
+   await Get.find<RecommendedProductController>().getRecommendedProductList();
+  }
+
   @override
   void initState() {
     super.initState();
+    _loadResources();
     controller =
         AnimationController(vsync: this, duration: const Duration(seconds: 2))..forward();
     animation = CurvedAnimation(parent: controller, curve: Curves.linear);
@@ -41,13 +51,13 @@ class _SplashScreenState extends State<SplashScreen>
             child: Center(
                 child: Image.asset(
               "assets/image/logo part 1.png",
-              width: 250,
+              width: Dimensions.splashImg,
             )),
           ),
           Center(
               child: Image.asset(
                 "assets/image/logo part 2.png",
-                width: 250,
+                width: Dimensions.splashImg,
               ))
         ],
       ),
