758b2aa1a156480633528c3b9cbbdc1422a1d4b7
Ayiks <cliffysarp@gmail.com>
2022-05-19 12:06:10 +0000

    cart history

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -239,6 +239,7 @@ class CartPage extends StatelessWidget {
                   GestureDetector(
                     onTap: () {
                       // popularProduct.addItem(product);
+                      cartController.addToHistory();
                     },
                     child: Container(
                       padding: EdgeInsets.only(
